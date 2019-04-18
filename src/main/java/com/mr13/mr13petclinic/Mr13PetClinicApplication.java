b5680a8f3a952411094e2c353faf4b9a1e5fdd20
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-18 17:28:06 +0300

    Initial Project Add

---
+++ src/main/java/com/mr13/mr13petclinic/Mr13PetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.mr13.mr13petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mr13PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mr13PetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mr13/mr13petclinic/Mr13PetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mr13.mr13petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mr13PetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
