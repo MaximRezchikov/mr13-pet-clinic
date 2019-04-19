b16ecd1eb323d94875f60c4dca989ab888eb0ca1
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-19 14:30:07 +0300

    Adding Index Page and controller.Closes #8

---
+++ pet-clinic-web/src/main/java/com/mr13/mr13petclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.mr13.mr13petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+
+        return "index";
+
+    }
+
+}
