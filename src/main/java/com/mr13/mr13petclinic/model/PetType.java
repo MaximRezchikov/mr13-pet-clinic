2dfa59f0df0cb105c6628724c53ff5978ba70594
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-19 08:35:12 +0300

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/mr13/mr13petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package com.mr13.mr13petclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/mr13/mr13petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.mr13.mr13petclinic.model;
+
+public class Vet extends Person {
+}
