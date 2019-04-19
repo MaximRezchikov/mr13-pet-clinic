89bae843d8bfcad9cff8bdf20b2ce2d57252d39b
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-19 15:31:22 +0300

    Adding Vets Page and controller.Closes #7

---
+++ pet-clinic-web/src/main/java/com/mr13/mr13petclinic/controllers/VetController.java
@@ -0,0 +1,14 @@
+package com.mr13.mr13petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+
+}
