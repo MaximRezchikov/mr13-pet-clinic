08e98c1a2da7adc70a3bae4a93b51ab553884832
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-19 13:25:16 +0300

    impl Map based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/map/OpenServiceMap.java
@@ -0,0 +1,34 @@
+package com.mr13.mr13petclinic.services.map;
+
+import com.mr13.mr13petclinic.model.Owner;
+import com.mr13.mr13petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OpenServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package com.mr13.mr13petclinic.services.map;
+
+import com.mr13.mr13petclinic.model.Pet;
+import com.mr13.mr13petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+}
