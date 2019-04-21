a6975625ebb8d3703bd76376a74fc367d652f1ec
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-21 15:07:21 +0300

    Update Map Impl to manage setting of ID Property.Closes #23

--- pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package com.mr13.mr13petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.mr13.mr13petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +16,15 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
 
         return object;
     }
@@ -29,6 +35,19 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+
+        Long nexId = null;
+
+        try {
+            nexId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nexId = 1L;
+        }
+
+        return nexId;
     }
 
 
