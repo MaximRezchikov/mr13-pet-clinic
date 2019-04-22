0107ce5816fd1aaa51dc22dc6e89df1d9ef421cf
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-22 19:08:04 +0300

    Added visits, update owner with pet relationship. Closes #26

--- pet-clinic-data/src/main/java/com/mr13/mr13petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package com.mr13.mr13petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
