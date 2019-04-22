0e2de594d2c90d2d0fe953b4efdf8c37e82404c7
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-22 19:27:44 +0300

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package com.mr13.mr13petclinic.services;
+
+import com.mr13.mr13petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.mr13.mr13petclinic.services.map;
+
+import com.mr13.mr13petclinic.model.PetType;
+import com.mr13.mr13petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
