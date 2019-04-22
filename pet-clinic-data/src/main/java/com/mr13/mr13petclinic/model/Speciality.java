ea1aaca7e23096f9213483dae217738f92fdbcca
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-22 19:12:29 +0300

    Added Vet Specialty, Associate To Vet Closes #27

---
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.mr13.mr13petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
