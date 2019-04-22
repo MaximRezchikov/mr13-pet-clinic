f3117be4ea3750f1488e6fc6939d0377304e6e85
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-04-22 20:33:33 +0300

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.mr13.mr13petclinic.services;
+
+import com.mr13.mr13petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package com.mr13.mr13petclinic.services.map;
+
+import com.mr13.mr13petclinic.model.Speciality;
+import com.mr13.mr13petclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
