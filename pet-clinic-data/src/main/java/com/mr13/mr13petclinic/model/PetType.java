1b039d709d798a922900f756d0ba9ae3d17fdff7
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-05-01 00:30:24 +0300

    Convert Owners to JPA Entities. Closes #36

--- pet-clinic-data/src/main/java/com/mr13/mr13petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/model/PetType.java
@@ -1,7 +1,14 @@
 package com.mr13.mr13petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType  extends BaseEntity {
 
+    @Column(name = "first_name")
     private String name;
 
     public String getName() {
