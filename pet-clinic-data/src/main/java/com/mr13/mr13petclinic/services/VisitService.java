73116b25a44d79316a47f9de1f176e53c2260d28
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-05-02 00:35:30 +0300

    Create Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package com.mr13.mr13petclinic.services;
+
+import com.mr13.mr13petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/mr13/mr13petclinic/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package com.mr13.mr13petclinic.services.map;
+
+import com.mr13.mr13petclinic.model.Visit;
+import com.mr13.mr13petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
