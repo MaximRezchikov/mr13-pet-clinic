0f8522bc3ca573647b3d58325cf0c7e9662c89c6
Maxim Rezchikov <maxim13rezchikov@gmail.com>
2019-05-12 14:50:13 +0300

    Upgraded to JUnit 5. Closes #4

--- pet-clinic-web/src/test/java/mr13petclinic/Mr13PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/mr13petclinic/Mr13PetClinicApplicationTests.java
@@ -1,12 +1,13 @@
 package mr13petclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootConfiguration
 @SpringBootTest
 public class Mr13PetClinicApplicationTests {
